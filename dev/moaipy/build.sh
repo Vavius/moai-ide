ac3e6868bb1c2c7d90164db4caf2216c51678368
Vavius <vavius@gmail.com>
2014-09-16 22:44:56 +0400

    change python path to default installation path (not using embedded OS X python, but official binary release since it was compiled for 10.6+)

--- dev/moaipy/build.sh
+++ dev/moaipy/build.sh
@@ -3,6 +3,6 @@
 # build python extension
 rm -f moaipy.cpp
 
-PYTHON_PATH="/usr/local/Cellar/python/2.7.3/bin/python"
+PYTHON_PATH="/Library/Frameworks/Python.framework/Versions/2.7/bin/python"
 export MACOSX_DEPLOYMENT_TARGET=10.8
 $PYTHON_PATH setup.py build
