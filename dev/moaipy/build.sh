2cba376442ac3e11fc11485c9282d2ada6052bac
Vasiliy Yanushevich <vavius@gmail.com>
2014-09-16 13:44:32 +0400

    new lib

--- dev/moaipy/build.sh
+++ dev/moaipy/build.sh
@@ -2,4 +2,7 @@
 
 # build python extension
 rm -f moaipy.cpp
+
+# export CFLAGS=-sysroot,/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.7.sdk
+# export LDFLAGS=-L/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.7.sdk/usr/lib
 python setup.py build
