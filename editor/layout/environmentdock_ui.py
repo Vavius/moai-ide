d7a5231c502b69fa34ef8f15687dd14fc48b5915
Vasiliy Yanushevich <vavius@gmail.com>
2014-12-26 19:07:09 +0300

    lib update

--- editor/layout/environmentdock_ui.py
+++ editor/layout/environmentdock_ui.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'environmentdock.ui'
 #
-# Created: Mon Nov 17 18:36:59 2014
+# Created: Fri Dec 26 14:28:38 2014
 #      by: pyside-uic 0.2.15 running on PySide 1.2.1
 #
 # WARNING! All changes made in this file will be lost!
